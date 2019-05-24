7c846a88ca103286990492b9d4db3483cd3db80b
ViktorKorladinov <v.korladinov@gmail.com>
2019-05-24 20:24:22 +0200

    Put some foundation towards login and register forms.

---
+++ src/components/Login.js
@@ -0,0 +1,18 @@
+import React, {Component} from 'react';
+
+
+class Login extends Component {
+    static defaultProps = {};
+
+    static propTypes = {};
+
+    state = {};
+
+    render() {
+        return (
+            <div>Hej</div>
+        );
+    }
+}
+
+export default Login;
--- src/components/NavigationBar.js
+++ src/components/NavigationBar.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import '../styles/NavigationBar.scss'
 import {connect} from "react-redux";
+import Login from "./Login";
 
 
 class NavigationBar extends Component {
@@ -8,18 +9,49 @@ class NavigationBar extends Component {
 
     static propTypes = {};
 
-    state = {};
+    state = {
+        status: 'none'
+    };
+
+    renderContent() {
+        switch (this.state.status) {
+            case 'log':
+                return <Login/>;
+            case 'reg':
+                return 'register';
+            case "none":
+            default:
+                return ''
+        }
+    }
+
+    change(status) {
+        if (status !== this.state.status) {
+            this.setState({status: status})
+        } else this.setState({status: 'none'});
+        console.log(this.state)
+    }
 
     render() {
-
         return (
             <div className={this.props.navStatus}>
-                <div className="nav-item"/>
+                <div className="nav-item">
+                    <span onClick={() => {
+                        this.change('log')
+                    }} className="log">Login</span>
+                    <span onClick={() => {
+                        this.change('reg')
+                    }} className="reg">Register</span>
+                </div>
+                <div className="nav-content">
+                    {this.renderContent()}
+                </div>
             </div>
         );
     }
 }
+
 const mapStateToProps = state => ({
     navStatus: state.internal.navStatus
 });
-export default connect(mapStateToProps,null)(NavigationBar);
+export default connect(mapStateToProps, null)(NavigationBar);
